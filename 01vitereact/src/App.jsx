acc3962e21f4ccef7b4b665e716300ee77ccf1c3
OmKumar <omk2357@gmail.com>
2023-11-14 17:45:19 +0530

    progect going On in react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <h1>Heloo Om</h1>
+    </>
+  );
+}
+
+export default App;
